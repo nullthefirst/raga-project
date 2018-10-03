b1bc27882b6098ea5e45bc0ff1b4e34d3b34eb89
Usheninte <jrandangana@gmail.com>
2018-10-03 14:53:12 +0100

    added GYLGF links

--- src/components/views/end.js
+++ src/components/views/end.js
@@ -7,7 +7,7 @@ export default class End extends Component {
     return(
       <div>
         <h3>Our Vision</h3>
-        <p>The Global Youth leadership and Girl-Child Foundation (GYLGF) envisions a society where the potentials of youth and girl-child are fully developed and supported to become socially responsible, responsive, and able to lead themselves and contribute meaningfully to a healthy society.</p>
+        <p>The <a href="https://www.gylgfoundation.org/" alt="Global Youth leadership and Girl-Child Foundation (GYLGF)">Global Youth leadership and Girl-Child Foundation (GYLGF)</a> envisions a society where the potentials of youth and girl-child are fully developed and supported to become socially responsible, responsive, and able to lead themselves and contribute meaningfully to a healthy society.</p>
       </div>
     );
   }
--- src/components/views/top.js
+++ src/components/views/top.js
@@ -7,7 +7,7 @@ export default class Top extends Component {
     return(
       <div>
         <h3>About Us</h3>
-        <p>The Global Youth leadership and Girl-Child Foundation (GYLGF) is a Non-Governmental and Leadership Organisation established in Nigeria, since 2001 and incorporated in 2014 with the Corporate Affairs Commission (CAC).</p>
+        <p>The <a href="https://www.gylgfoundation.org/" alt="Global Youth leadership and Girl-Child Foundation (GYLGF)">Global Youth leadership and Girl-Child Foundation (GYLGF)</a> is a Non-Governmental and Leadership Organisation established in Nigeria, since 2001 and incorporated in 2014 with the Corporate Affairs Commission (CAC).</p>
         <br />
         <p>The foundation is concerned with providing result-based and enduring solutions to the challenges faced by women, youths and the girl-child especially in Oyo State, Nigeria and in any part of the World.</p>
       </div>
