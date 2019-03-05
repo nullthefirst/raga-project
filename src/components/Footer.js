20b386c9ebf355fa83cbbb76538241f7ad63e197
Usheninte <jrandangana@gmail.com>
2019-03-05 09:30:17 +0100

    editions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 
 import './Main.css';
 
-const footerStyle = {
-  textAlign: "center",
-  marginTop: "2em"
-};
+const titleMinor2 = {
+  padding: '1em',
+  fontSize: '2em',
+  fontWeight: 'bold'
+}
 
 export default class Footer extends Component {
   render() {
     return(
-      <div style={footerStyle} className="App-link">
+      <div style={titleMinor2} className="App-link">
         <h5>GYLGF &copy; 2018 - 2019 | All Rights Reserved</h5>
-        <h6><a href="https://twitter.com/Usheninte">Coded by Usheninte</a></h6>
+        <h6><a href="https://usheninte.xyz">Coded by Usheninte</a></h6>
       </div>
     );
   }
