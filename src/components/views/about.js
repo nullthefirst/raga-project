7b65a9eec18a78477ab42cfedb84f9a3b7e6a343
Usheninte <jrandangana@gmail.com>
2019-03-05 09:22:26 +0100

    implementing design

--- src/components/views/about.js
+++ src/components/views/about.js
@@ -10,11 +10,19 @@ const blackBox = {
   backgroundColor: '#212121'
 }
 
+const titleDark = {
+  fontSize: '2em',
+  fontWeight: 'bold',
+  paddingTop: '0.5em',
+  textDecoration: 'underline',
+  textDecorationColor: '#3cb371'
+}
+
 export default class About extends Component {
   render() {
     return(
       <div style={blackBox}>
-        <h3>About Us</h3>
+        <h3 style={titleDark}>About Us</h3>
         <p>The <a href="https://www.gylgfoundation.org/" alt="Global Youth leadership and Girl-Child Foundation (GYLGF)">Global Youth leadership and Girl-Child Foundation (GYLGF)</a> is a Non-Governmental and Leadership Organisation established in Nigeria, since 2001 and incorporated in 2014 with the Corporate Affairs Commission (CAC).</p>
         <br />
         <p>The foundation is concerned with providing result-based and enduring solutions to the challenges faced by women, youths and the girl-child especially in Oyo State, Nigeria and in any part of the World.</p>
--- src/components/views/vision.js
+++ src/components/views/vision.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 import '../Main.css';
 
+const titleMinor = {
+  fontSize: '2em',
+  fontWeight: 'bold'
+}
+
 export default class Vision extends Component {
   render() {
     return(
       <div>
-        <h3>Our Vision</h3>
+        <h3 style={titleMinor}>Our Vision</h3>
         <p>The <a href="https://www.gylgfoundation.org/" alt="Global Youth leadership and Girl-Child Foundation (GYLGF)">Global Youth leadership and Girl-Child Foundation (GYLGF)</a> envisions a society where the potentials of youth and girl-child are fully developed and supported to become socially responsible, responsive, and able to lead themselves and contribute meaningfully to a healthy society.</p>
       </div>
     );
