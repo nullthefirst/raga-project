68977b81a3020497affc4c0f342772399ecaf63e
Ninte Dangana <ninte.dev@gmail.com>
2021-09-20 10:33:08 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export default class Footer extends Component {
   render() {
     return(
       <div style={titleMinor2} className="App-link">
-        <h5>Global Youth leadership and Girl-Child Foundation (GYLGF) &copy; All Rights Reserved</h5>
+        <h5>Global Youth leadership and Girl-Child Foundation (GYLGF) &copy; All Rights Reserved [2018-2021]</h5>
         <h6><a href="https://ninte.dev">ninte.dev</a></h6>
       </div>
     );
